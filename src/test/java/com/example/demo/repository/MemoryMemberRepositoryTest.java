af4aefe9fb7123142c78d01cfc531716928bbb62
onyoo <dhsdb02@gmail.com>
2022-12-19 12:23:15 +0900

    test(core): Add MemberRepository test code

---
+++ src/test/java/com/example/demo/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,59 @@
+package com.example.demo.repository;
+
+import com.example.demo.domain.Member;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class MemoryMemberRepositoryTest {
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @AfterEach
+    public void afterEach(){
+        repository.clearStore();
+        //각각의 테스트 코드의 의존성을 없애기 위한 clear 코드
+    }
+
+    @Test
+    public void save(){
+        Member member = new Member();
+        member.setName("spring");
+
+        repository.save(member);
+
+        Member result = repository.findById(member.getId()).get();
+        assertThat(member).isEqualTo(result);
+
+    }
+
+    @Test
+    public void findByName(){
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        Member result = repository.findByName("spring1").get();
+        assertThat(result).isEqualTo(member1);
+        //member1 vs member2
+    }
+
+    public void findAll(){
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+
+
+    }
+
+
+}
