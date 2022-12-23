94a3790b273e11ddd89387a563d11efcc2a60f9c
onyoo <dhsdb02@gmail.com>
2022-12-23 15:28:23 +0900

    feat: Add springConfig

---
+++ src/main/java/com/example/demo/SpringConfig.java
@@ -0,0 +1,37 @@
+package com.example.demo;
+
+import com.example.demo.repository.JdbcMemberRepository;
+import com.example.demo.repository.MemberRepository;
+import com.example.demo.repository.MemoryMemberRepository;
+import com.example.demo.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import javax.xml.crypto.Data;
+
+@Configuration
+public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource){
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new JdbcMemberRepository(dataSource);
+        //return new MemoryMemberRepository();
+
+    }
+
+}
+//config를 작성하여 직접 등록하는 방법
